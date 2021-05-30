52f7a831468844e69d700f27d7b790a2c1b840c1
LucunJi <lucunjif@gmail.com>
2021-05-29 21:40:19 -0500

    change the name of radiative item model template
    change moon item texture
    
    add recipe for evil eye
    
    add loot table for fire stone

--- src/main/java/io/github/lucunji/uusiaurinko/mixins/EntityMixinClientOnly.java
+++ src/main/java/io/github/lucunji/uusiaurinko/mixins/EntityMixinClientOnly.java
@@ -10,6 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixinClientOnly {
+
+    /**
+     * The player with true vision effect can see invisible entities.
+     */
     @Inject(method = "isInvisibleToPlayer", at = @At("RETURN"), cancellable = true)
     private void onIsInvisibleToPlayerInvoked(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
         if (player.isPotionActive(ModEffects.TRUE_VISION.get()))
