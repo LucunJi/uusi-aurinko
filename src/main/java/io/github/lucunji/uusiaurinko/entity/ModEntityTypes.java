bc747aca2e426c0a68a8b7627e2f319d6142d765
LucunJi <lucunjif@gmail.com>
2021-07-03 18:14:11 -0500

    remove redundant register methods

--- src/main/java/io/github/lucunji/uusiaurinko/entity/ModEntityTypes.java
+++ src/main/java/io/github/lucunji/uusiaurinko/entity/ModEntityTypes.java
@@ -7,44 +7,33 @@ import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
 
 import static io.github.lucunji.uusiaurinko.UusiAurinko.MODID;
 
 public class ModEntityTypes {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, MODID);
 
-    public static final RegistryObject<EntityType<RadiativeItemEntity>> RADIATIVE_ITEM = register("radiative_item",
+    public static final RegistryObject<EntityType<RadiativeItemEntity>> RADIATIVE_ITEM = ENTITY_TYPES.register("radiative_item",
             () -> EntityType.Builder.<RadiativeItemEntity>create(RadiativeItemEntity::new, EntityClassification.MISC)
                     .size(0.25f, 0.25f).trackingRange(10).updateInterval(10).build("radiative_item"));
-
-    private static <T extends Entity> RegistryObject<EntityType<T>> register(
-            final String name, final Supplier<EntityType<T>> sup) {
-
-        RegistryObject<EntityType<T>> registryObject = ENTITY_TYPES.register(name, sup);
-        return registryObject;
-    }
 
     @OnlyIn(Dist.CLIENT)
     public static class ClientRenderer {
         public static final List<Runnable> RENDERER_BINDERS = new ArrayList<>();
 
         static {
-            register(RADIATIVE_ITEM, manager -> new RadiativeItemEntityRenderer(manager, Minecraft.getInstance().getItemRenderer()));
+            registerRenderer(RADIATIVE_ITEM, manager -> new RadiativeItemEntityRenderer(manager, Minecraft.getInstance().getItemRenderer()));
         }
 
-        public static <T extends Entity> void register(RegistryObject<EntityType<T>> registryObject, IRenderFactory<? super T> renderFactory) {
+        public static <T extends Entity> void registerRenderer(RegistryObject<EntityType<T>> registryObject, IRenderFactory<? super T> renderFactory) {
             RENDERER_BINDERS.add(() -> RenderingRegistry.registerEntityRenderingHandler(registryObject.get(), renderFactory));
         }
     }
--- src/main/java/io/github/lucunji/uusiaurinko/tileentity/ModTileEntityTypes.java
+++ src/main/java/io/github/lucunji/uusiaurinko/tileentity/ModTileEntityTypes.java
@@ -1,29 +1,21 @@
 package io.github.lucunji.uusiaurinko.tileentity;
 
 import io.github.lucunji.uusiaurinko.block.ModBlocks;
-import io.github.lucunji.uusiaurinko.client.render.entity.RadiativeItemEntityRenderer;
 import io.github.lucunji.uusiaurinko.client.render.tileentity.TransmutingTileEntityRenderer;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.client.registry.IRenderFactory;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static io.github.lucunji.uusiaurinko.UusiAurinko.MODID;
 
@@ -31,26 +23,19 @@ import static io.github.lucunji.uusiaurinko.UusiAurinko.MODID;
 public class ModTileEntityTypes {
     public final static DeferredRegister<TileEntityType<?>> TILE_ENTITY = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, MODID);
 
-    public final static RegistryObject<TileEntityType<TransmutingTileEntity>> TRANSMUTING_BLOCK = register("transmuting_block",
+    public final static RegistryObject<TileEntityType<TransmutingTileEntity>> TRANSMUTING_BLOCK = TILE_ENTITY.register("transmuting_block",
             () -> TileEntityType.Builder.create(TransmutingTileEntity::new, ModBlocks.TRANSMUTING_BLOCK.get()).build(null));
-
-    private static <T extends TileEntity> RegistryObject<TileEntityType<T>> register(
-            final String name, final Supplier<TileEntityType<T>> sup) {
-
-        RegistryObject<TileEntityType<T>> registryObject = TILE_ENTITY.register(name, sup);
-        return registryObject;
-    }
 
     @OnlyIn(Dist.CLIENT)
     public static class ClientRenderer {
         public static final List<Runnable> RENDERER_BINDERS = new ArrayList<>();
 
         static {
-            register(TRANSMUTING_BLOCK, TransmutingTileEntityRenderer::new);
+            registerRenderer(TRANSMUTING_BLOCK, TransmutingTileEntityRenderer::new);
         }
 
-        public static <T extends TileEntity> void register(RegistryObject<TileEntityType<T>> registryObject,
-                                                       Function<? super TileEntityRendererDispatcher, TileEntityRenderer<? super T>> rendererFactory) {
+        public static <T extends TileEntity> void registerRenderer(RegistryObject<TileEntityType<T>> registryObject,
+                                                                   Function<? super TileEntityRendererDispatcher, TileEntityRenderer<? super T>> rendererFactory) {
             RENDERER_BINDERS.add(() -> ClientRegistry.bindTileEntityRenderer(registryObject.get(), rendererFactory));
         }
     }
